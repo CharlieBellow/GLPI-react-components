c73214498eac75e4607836398701f541718c354c
Humberto Barbosa <humbertob.tavares@gmail.com>
2022-10-05 14:33:37 -0300

    fix(components): altera medidas para a base 4

--- src/components/ButtonCardAction.tsx
+++ src/components/ButtonCardAction.tsx
@@ -8,7 +8,7 @@ export function ButtonCardAction(props: ButtonProps) {
   return (
     <button
       className={`hover:bg-opacity-80 focus:outline-azul-ufal font-bold py-2
-      rounded-md shadow-sombra-botao text-base px-[0.625rem] h-[2.625rem]
+      rounded-md shadow-sombra-botao text-base px-3 h-10
       ${props.bg} ${props.textColor}`}
     >
       {props.title}
