ebd450ac16115c794a524c5f97dca92293a365fe
charlie@nti <charliebftm@gmail.com>
2022-10-10 11:40:52 -0300

    add cardLogin

--- src/Pages/Login/index.tsx
+++ src/Pages/Login/index.tsx
@@ -1,29 +1,25 @@
 //import {CardLabel} from "../../components/CardLabel";
-import CardLabelInput from "../../components/CardLabelInput";
+import { CardLogin } from '../../components/CardLogin'
 
 //ver como criar esse card de login, entender como estão organizados os cards
 function Login() {
   return (
 		<>
 			<div className="bg-gradiente w-screen h-screen fixed">
-				<div className="bg-backgroundUfal w-[39.961rem] opacity-30 h-[108.625rem] left-[-15.98%] fixed   top-[-44.83%] bottom-[-64.06%] right-[21.39%] bg-no-repeat"></div>
-			</div>
-      <div className="font-roboto mt-44  w-[400px] h-[474px] top-[]179px]  bg-branco-gelo rounded-lg shadow-sombra-card">
-        <div>
+				<div className="bg-backgroundUfal w-full opacity-30 h-[108.625rem] left-[-31rem] fixed top-[-40.83%] bottom-[-64.06%] right-[21.39%] bg-no-repeat">
 
-				<CardLabelInput label="email" inputId="email" width="320"/>
-        </div>
+				</div>
+			<CardLogin  />
 			</div>
+			<div
+				//className="font-roboto mt-44  w-[400px] h-[474px] top-[]179px]  bg-branco-gelo rounded-lg shadow-sombra-card"
+			>
+				
+			</div>
+			
 		</>
 	);
 }
 
 export default Login
 
-
-
-//position: absolute;
-//left: -15.98%;
-//right: 21.39%;
-//top: -44.83%;
-//bottom: -64.06%;
---
+++ src/components/CardLogin.tsx
@@ -0,0 +1,43 @@
+import "../styles/main.css";
+import { Button } from "./Button";
+import { CardTitle } from "./CardTitle";
+//import { CardLine } from "./CardLine";
+import { CardFloatingLabelInput } from "./CardFloatingLabelInput";
+
+export function CardLogin() {
+	return (
+		<div className="m-[4.5rem] mb-52 container w-[25rem] h-[32rem] mx-auto bg-branco-gelo rounded-lg shadow-sombra-card">
+			<div className="pt-[1.625rem] pb-8 text-center">
+				<CardTitle title="Criar Conta" />
+			</div>
+			<form action="">
+				<div className="mb-6">
+					<CardFloatingLabelInput
+						label="Nome Completo"
+						inputId="fullName"
+						width="w-full"
+					/>
+				</div>
+				<div className="mb-6">
+					<CardFloatingLabelInput
+						label="Email"
+						inputId="email"
+						width="w-full"
+					/>
+				</div>
+				<div className="mb-6">
+					<CardFloatingLabelInput
+						label="Senha"
+						inputId="password"
+						width="w-full"
+					/>
+				</div>
+			</form>
+      <div className="flex flex-col justify-center  mt-8 mx-11">
+        {/* quando colocar o estilo pela pros style ele não sobrescreve a estilização e aí dificulta o aproveitamento do componente  */}
+				<Button title="Cadastrar" style="btn-primary pb-4" />
+				<Button title="Fazer login" style="btn-text-only" />
+			</div>
+		</div>
+	);
+}
