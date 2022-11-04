98c12ed0fafd203f1123d3d52163c78774b43b16
Humberto Barbosa <humbertob.tavares@gmail.com>
2022-11-04 08:27:40 -0300

    feat: torna o ```CardFormService``` responsivo

--- src/components/CardFormService.tsx
+++ src/components/CardFormService.tsx
@@ -6,46 +6,51 @@ import { CardLabelTextarea } from "./CardLabelTextarea";
 
 function CardFormService() {
   return (
-		<div className="font-roboto mt-[4.5rem] mb-56 container w-[50.5rem] h-auto mx-auto bg-branco-gelo rounded-lg shadow-sombra-card ">
-			<div className="pl-9 pt-8">
-				<CardTitle title="Solicitar Serviço" />
-			</div>
-			<div className="mx-9 mt-4 mb-10">
-				<CardLine />	
-			</div>
-			<form action="" className="">
-				<div className="mx-14 pb-10">
-					<CardLabelInput
-						label="Título"
-						type="text"
-						inputId="title"
-						width="w-full"
-					/>
+		<div className='mx-4'>
+			<div
+				className="font-roboto mt-[4.5rem] mx-auto mb-80 flex flex-col lg:block
+				bg-branco-gelo pb-9 rounded-lg max-w-2xl lg:max-w-[50.5rem] lg:w-[50.5rem] h-auto shadow-sombra-card"
+			>
+				<div className="pl-9 pt-8">
+					<CardTitle title="Solicitar Serviço" />
 				</div>
-				<div className="mx-14">
-					<CardLabelInput
-						label="Local do serviço"
-						type="text"
-						inputId="title"
-						width="w-full"
-					/>
-				</div>
-				<div className="mt-9">
-					<CardLabelTextarea
-						label="Descrição"
-						textareaId="description"
-					/>
-				</div>
-			</form>
-			<div className="flex justify-end gap-x-3.5 mr-14 mt-10 pb-10">
-				<Button
-					title="Solicitar"
-					style={`btn-primary-action`}
-				/>
-				<Button
-					title="Cancelar"
-					style={`btn-secondary-action`}
-				/>
+				<div className="mx-9 mt-4 mb-10">
+		 			<CardLine />	
+		 		</div>
+				<form action="" className="">
+		 			<div className='flex flex-col gap-9'>
+			 			<div className='mx-14'>
+			 				<CardLabelInput
+			 					label="Título"
+			 					type="text"
+			 					inputId="title"
+			 					width="w-full"	
+			 				/>
+			 			</div>
+			 			<div className='mx-14'>
+			 				<CardLabelInput
+			 					label="Local do serviço"
+			 					type="text"
+			 					inputId="title"
+			 					width="w-full"
+			 				/>
+			 			</div>
+			 			<CardLabelTextarea
+			 				label="Descrição"
+			 				textareaId="description"
+			 			/>
+			 		</div>
+			 	</form>
+			 	<div className="flex justify-end gap-x-3.5 mr-14 mt-10">
+			 		<Button
+			 			title="Solicitar"
+			 			style={`btn-primary-action`}
+			 		/>
+			 		<Button
+			 			title="Cancelar"
+			 			style={`btn-secondary-action`}
+			 		/>
+			 	</div>
 			</div>
 		</div>
 	);
