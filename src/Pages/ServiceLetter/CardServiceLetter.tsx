a7fd6127314708270ce16e837a5cc608c9902d82
charlie@nti <charliebftm@gmail.com>
2022-11-17 16:53:45 -0300

    add IfoService component

--- src/Pages/ServiceLetter/CardServiceLetter.tsx
+++ src/Pages/ServiceLetter/CardServiceLetter.tsx
@@ -1,11 +1,19 @@
 import { InfoService } from "./InfoService";
 
+
+
+import { Target } from "phosphor-react";
+
 export function CardServiceLetter() {
   return (
-    <>
-      <div>
-        <InfoService/>
-      </div>
-    </>
-  )
+		<>
+			<div>
+				<InfoService
+					icon={<Target />}
+					title="público-Alvo"
+					description="loremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremlorem"
+				/>
+			</div>
+		</>
+	);
 }
--- src/Pages/ServiceLetter/InfoService.tsx
+++ src/Pages/ServiceLetter/InfoService.tsx
@@ -1,32 +1,38 @@
 
-import { Target } from "phosphor-react";
+//import { Icon, Target } from "phosphor-react";
+import { ReactNode } from "react";
+
+interface InfoServiceProps {
+	title: string;
+	icon: ReactNode;
+	description: string;
+
+
+
+}
 
 
 // depois fazer um map nesse componente e sair colocando todas as informações como props (titulo da informação e descrição) aí cada chamado vai ter seu objeto com as informações necessárias.a´i já atribui um ícone padrão pra cada tipo de informação
 
 // estilizar o acordeon
-export function InfoService() {
-	
+export function InfoService(props: InfoServiceProps) {
 	return (
 		<>
 			<div className="  mt-3 mx-4">
 				<div className="flex gap-2 items-center">
-					<Target weight="bold" />
-					<h3 className="text-lg font-bold">Público-Alvo</h3>
+					{/*<Target weight="bold" />*/}
+					{ props.icon}
+					<h3 className="text-lg font-bold">{props.title}</h3>
 				</div>
-				<input type="checkbox" name="moreText" id="moreText" className="hidden"/>
+				<input
+					type="checkbox"
+					name="moreText"
+					id="moreText"
+					className="hidden"
+				/>
 				<div className="toggleCheck ">
 					<p className="check">
-						Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cum, quos
-						assumenda id consequuntur quaerat reprehenderit fuga aliquid
-						provident sapiente officia eveniet, sunt eligendi nesciunt
-						voluptates repudiandae ea vel eum quam! Lorem ipsum, dolor sit amet
-						consectetur adipisicing elit. Sit deleniti quia nobis dicta sed unde
-						eius, illo laborum nesciunt est eveniet delectus molestiae, quas
-						neque voluptatem modi harum, corrupti temporibus? Lorem ipsum dolor
-						sit amet, consectetur adipisicing elit. Possimus et perspiciatis
-						aliquam, obcaecati numquam illo delectus ut \afdawgewrgege\a iatis
-						aliquam, obcaecati numquam illo delectus ut \afdawgewrgege\a
+						{ props.description}
 					</p>
 
 					<label htmlFor="moreText" className="label ">
