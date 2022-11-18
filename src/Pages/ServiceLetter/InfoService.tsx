46a92915455bc93b84b050aaf8c96c07beaeca39
charlie@nti <charliebftm@gmail.com>
2022-11-18 15:56:40 -0300

    improvements on search component

--- src/Pages/ServiceLetter/InfoService.tsx
+++ src/Pages/ServiceLetter/InfoService.tsx
@@ -10,6 +10,7 @@ interface InfoServiceItemProps {
 
 // estilizar o acordeon
 export function InfoServiceItem(props: InfoServiceItemProps) {
+	//const altura =  height <= 70rem ? showButton : hideButton
 	return (
 		<>
 			{props.infos.map(info => {
@@ -22,16 +23,20 @@ export function InfoServiceItem(props: InfoServiceItemProps) {
 						<input
 							type="checkbox"
 							name="moreText"
-							id={info.title }
+							id={info.title}
 							className="hidden"
 						/>
 						<div className="toggleCheck ">
 							<p className="check">{info.description}</p>
 
 							<label htmlFor={info.title} className="label ">
-								<p className="text-end text-blue-ufal underline pt-1">
-									<strong className="show">...Mostrar mais</strong>
-									<strong className="noShow">Mostrar menos</strong>
+								<p className="text-end text-blue-ufal  underline pt-1">
+									<strong className="show hover:opacity-70">
+										...Mostrar mais
+									</strong>
+									<strong className="noShow hover:opacity-70">
+										Mostrar menos
+									</strong>
 									<div className="border-b-2 border-gray-text"></div>
 								</p>
 							</label>
