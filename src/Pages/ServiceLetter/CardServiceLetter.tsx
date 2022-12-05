56d835e8f9c709111dff15ff1147aaae4cda4f59
charlie@nti <charliebftm@gmail.com>
2022-12-05 10:58:28 -0300

    catch button add service

--- src/Pages/ServiceLetter/CardServiceLetter.tsx
+++ src/Pages/ServiceLetter/CardServiceLetter.tsx
@@ -1,5 +1,6 @@
 import { InfoServiceItem } from "./InfoService";
 import * as Icon from 'phosphor-react'
+import { Button } from "../../components/Button";
 
 const servicesList = [
 	{
@@ -55,16 +56,39 @@ export function CardServiceLetter(props: CardServiceLetterProps) {
 	return (
 		<>
 			<div className="lg:bg-white-100 lg:mx-10 lg:rounded-lg lg:px-8 lg:py-8 lg:my-8 md:mx-16 text-justify">
-				<div className="flex  justify-between">
-					<h3 className="font-3xl pt-4 font-extrabold ml-4 text-2xl lg:text-3xl lg:flex">
+				<div className="lg:flex lg:justify-between lg:items-baseline">
+					<h3 className="pt-4 font-medium ml-4 text-3xl lg:text-4xl lg:flex">
 						{props.serviceTitle}
 					</h3>
-					<button className="hidden lg:flex">Solicitar serviço</button>
+					<div className="fixed top-[630px] right-2 lg:right-22 lg:top-202">
+						<Button
+							title={floatingButton ? "" : "Solicitar serviço"}
+							theme="withIcon"
+							icon={<Icon.PhoneOutgoing size={24} />}
+						/>
+					</div>
 				</div>
 				<InfoServiceItem
-					icon={<Icon.Target size={20} weight="bold" className=""/>}
+					icon={<Icon.Target size={20} weight="bold" className="" />}
 					infos={servicesList}
 				/>
+				<div className="ml-4 mt-9 flex gap-3.5 lg:visible invisible">
+					<Button
+						title="Solicitar Serviço"
+						theme="primaryActionWithIcon"
+						icon={<Icon.PhoneOutgoing size={24} />}
+					/>
+					<Button
+						title="Esclarecer Dúvidas"
+						theme="secondaryActionWithIcon"
+						icon={<Icon.Question size={24} />}
+					/>
+					<Button
+						title="Relatar Problema"
+						theme="tertiaryActionWithIcon"
+						icon={<Icon.Warning size={24} />}
+					/>
+				</div>
 			</div>
 		</>
 	);
