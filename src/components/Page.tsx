7ce7b33f359cc7348e08c56a4cde8497fb6f0067
charlie@nti <charliebftm@gmail.com>
2022-12-06 16:42:17 -0300

    Page component improvements

--- src/components/Page.tsx
+++ src/components/Page.tsx
@@ -10,18 +10,18 @@ export function Page(props: PageProps) {
 	document.title = props.pagetitle;
 	return (
 		<>
-			<div className="layout__menu-header bg-white-strong-ice " {...props}>
+			<div className="layout__menu-header bg-white-strong-ice" {...props}>
 				<div className="flex max-w-13xl mx-auto" {...props}>
 					<div className="flex" {...props}>
 						<Menu />
 					</div>
 
-					<div className="pageContent w-full flex flex-col" {...props}>
+					<div className="pageContent w-full h-screen flex flex-col" {...props}>
 						<div {...props}>
 							<Header page={props.pagetitle} />
 						</div>
 
-						<div {...props} className="content">
+						<div {...props} className="content ">
 							{props.contentpage}
 						</div>
 					</div>
