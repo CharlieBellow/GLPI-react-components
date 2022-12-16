fa67fc124eb3516cebac1273dcb1b991af91dee4
charlie@nti <charliebftm@gmail.com>
2022-12-16 16:44:52 -0300

    add service component

--- src/components/Service.tsx
+++ src/components/Service.tsx
@@ -1,27 +1,25 @@
 export const servicesList = [{id: "00", name:"Socorro Jesus", title: "Preciso de ajuda!", description: "Infelizmente não dá pra gente ganhar todas, mas perder todas aparentemente dá sim.", serviceLocal: "Bloco A" }]
 
-export default function Service() {
+interface ServiceProps {
+	name: string;
+	title: string;
+	serviceLocal: string;
+	description: string;
+	id: string;
+}
+
+
+export default function Service(props: ServiceProps) {
 	return (
-		<div className="text-light-bg w-36 h-20 bg-white-ice pt-3 px-3 rounded-lg shadow-card lg:w-52 lg:h-full flex flex-col lg:gap-1 lg:py-4 lg:px-4 cursor-pointer">
-			<strong className="text-sm mt-3 font-medium lg:text-xl">Titulo:</strong>
-			<p className="text-sm mt-3 font-medium lg:text-xl">Nome: </p>
-			<p className="text-sm mt-3 font-medium lg:text-xl">Descrição: </p>
-			<p className="text-sm mt-3 font-medium lg:text-xl">Bloco: </p>
-			{/*<>
-          {servicesList.map((user: any) => {
-            return (
-              <User
-                nome={user.name}
-                date={user.date}
-                time={user.time}
-                email={user.email}
-                id={user.time}
-                key={user.time}
-              />
-            );
-          })}
-          {console.log(usersList)}
-        </>*/}
+    <div className="text-light-bg w-36 h-20 bg-white-ice pt-3 px-3 rounded-lg shadow-card lg:w-52 lg:h-full flex flex-col lg:gap-1 lg:py-4 lg:px-4 cursor-pointer" { ...props } key={ props.id } id={props.id}>
+      <strong className="text-sm mt-3 font-medium lg:text-xl">Titulo: {props.title}</strong>
+			<p className="text-sm mt-3 font-medium lg:text-xl">Nome: {props.name}</p>
+			<p className="text-sm mt-3 font-medium lg:text-xl">
+				Descrição: {props.description}
+			</p>
+			<p className="text-sm mt-3 font-medium lg:text-xl">
+				Bloco: {props.serviceLocal}
+			</p>
 		</div>
 	);
 }
--- src/components/Services.tsx
+++ src/components/Services.tsx
@@ -1,12 +1,26 @@
-import Service from "./Service";
+import { servicesList } from "./Service";
+import Service from "./Service"
 
 export default function Services () {
   return (
 		<div className="m-8 bg-white-100 gap-3">
+			<h2 className="text-5xl font-bold ">Serviços</h2>
 
-      <h2 className="text-5xl font-bold ">Serviços</h2>
-      
-			<Service/>
+			<>
+          {servicesList.map((user: any) => {
+            return (
+              <Service
+                name={user.name}
+                title={user.title}
+                description={user.description}
+                serviceLocal={user.serviceLocal}
+                id={user.id}
+                key={user.id}
+              />
+            );
+          })}
+          {console.log(servicesList)}
+        </>
 		</div>
 	);
 }
