f2e49e8ad3cbe2a6bfd0eb6551bdfdd8dd854a93
charlie@nti <charliebftm@gmail.com>
2023-01-30 16:54:00 -0300

    oeganizing the servicebook route

--- src/components/CardCategory.tsx
+++ src/components/CardCategory.tsx
@@ -1,15 +1,24 @@
 import * as Icon from "phosphor-react";
-import { Link } from "react-router-dom";
+import { Link} from "react-router-dom";
+import { categoryModel } from "../Utils/ServiceModels";
 
 interface categoriaProps {
 	Name: String;
 	Icon: Icon.IconProps;
 	link: string;
+	idCategory: string
 }
 
-function CardCategory(props: categoriaProps) {
+export default function CardCategory ( props: categoriaProps ) {
+	
+  const id = categoryModel.map( category =>
+    category.id === props.idCategory
+      ? category.titleCategory 
+      :  "" 
+  );
+
 	return (
-		<Link to={props.link}>
+    <Link to={props.link}>
 			<div className="text-light-bg w-36 h-20 bg-white-ice pt-3 px-3 rounded-lg shadow-card lg:w-52 lg:h-26 flex flex-col lg:gap-2 lg:pt-4 lg:px-4 cursor-pointer hover:bg-opacity-20">
 				<>{props.Icon}</>
 				<p className="text-sm mt-3 font-medium lg:text-xl">{props.Name}</p>
@@ -18,4 +27,4 @@ function CardCategory(props: categoriaProps) {
 	);
 }
 
-export default CardCategory;
+
