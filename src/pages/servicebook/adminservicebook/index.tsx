ef41af0a4713a79238b1df1949f9c5e02eeca6ee
Charlie Bellow <charliebftm@gmail.com>
2023-03-31 11:52:14 -0300

    Add Servicebook Administrator Screen

---
+++ src/pages/servicebook/adminservicebook/index.tsx
@@ -0,0 +1,39 @@
+import Head  from 'next/head';
+import React, { useEffect, useState } from 'react'
+import AdminServiceBookTypeList from './AdminServicebookTypeList';
+import { getAllGroups } from '../../../Utils/server/getInfo';
+import { getAllSubGroups } from '../../../Utils/server/getInfo';
+import { useRouter } from 'next/router';
+
+interface AdminProps {
+  list: any[];
+  title: string;
+}
+
+export default function AdminServiceBook(props: AdminProps) {
+//  fazer a requisição de todos os grupos, subgrupos e serviços
+  const [list, setList] = useState<[]>([])
+  const router = useRouter()
+
+  useEffect(() => {
+   
+    if (!router.isReady) return;
+    const fetchData = async () => {
+
+      const groups = await getAllGroups()
+      setList(groups)
+    }
+    fetchData()
+    console.log(list)
+  }, [router.isReady])
+  
+  
+  return (
+    <>
+      <Head>
+        <title>Administrar servicebook</title>
+      </Head>
+      <AdminServiceBookTypeList list={list} title={"Grupos"} />
+    </>
+  )
+}
