1a004e387b0449ba04b7b5ed8fa24481bcd6871f
Enrique Santos <santosenrique869@gmail.com>
2023-07-04 14:53:02 -0300

    refactor: remove trailingSlash option from nextConfig

--- next.config.js
+++ next.config.js
@@ -1,18 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  trailingSlash: true,
   reactStrictMode: true,
   images: {
     remotePatterns: [
-        {
-          protocol: 'https',
-          hostname: 'www.github.com',
-          port: '',
-          pathname: '/**',
-        },
-      ],
+      {
+        protocol: "https",
+        hostname: "www.github.com",
+        port: "",
+        pathname: "/**",
+      },
+    ],
   },
 };
 
-
-module.exports = nextConfig
+module.exports = nextConfig;
