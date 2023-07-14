77baa0039b12bf36ba6cb373d7136ea2b61cb944
Enrique Santos <santosenrique869@gmail.com>
2023-07-14 10:14:18 -0300

    fix: image logo UFAL not respect position

--- src/app/(public)/(auth)/layout.tsx
+++ src/app/(public)/(auth)/layout.tsx
@@ -2,8 +2,16 @@ import { ReactNode } from "react";
 
 export default function AuthLayout({ children }: { children: ReactNode }) {
   return (
-    <main className="grid h-full place-content-center bg-gradient">
+    <main className="grid h-full place-content-center overflow-hidden bg-gradient">
       {children}
+      <div className="absolute left-0 top-1/2 flex h-screen w-full -translate-y-1/2 items-center justify-start overflow-hidden">
+        {/* eslint-disable-next-line @next/next/no-img-element */}
+        <img
+          className="block scale-[250%] pr-20 pt-20 sm:scale-[200%] sm:pt-20 md:scale-125 md:pr-36 md:pt-32 lg:scale-100 lg:pr-56 lg:pt-36 tv:scale-90 tv:pr-72 tv:pt-40"
+          src="/images/ufal-sigla-branca-fundo-transparente-40por-cento.png"
+          alt="brasão da UFAL com fundo transparente"
+        />
+      </div>
     </main>
   );
 }
--- src/app/(public)/(auth)/signup/page.tsx
+++ src/app/(public)/(auth)/signup/page.tsx
@@ -1,6 +1,4 @@
 import { Metadata } from "next";
-import Image from "next/image";
-
 import { FormSignUp } from "./components/FormSignUp";
 
 export const metadata: Metadata = {
@@ -9,16 +7,7 @@ export const metadata: Metadata = {
 
 export default function SignUpPage() {
   return (
-    <div className="mx-auto flex  h-screen w-screen flex-col items-center justify-center gap-8">
-      <div className="relative right-128 h-screen w-full">
-        <Image
-          className=" fixed -bottom-118 h-auto"
-          src="/images/ufal-sigla-branca-fundo-transparente-40por-cento.png"
-          width="1300"
-          height="500"
-          alt="brasão da UFAL com fundo transparente"
-        />
-      </div>
+    <div className="z-10 mx-auto flex h-screen w-screen flex-col items-center justify-center gap-8">
       <div className="absolute flex h-full w-full">
         <FormSignUp />
       </div>
