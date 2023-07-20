535729a802a0ea2ec69de717b32ffdd433d11a27
Enrique Santos <santosenrique869@gmail.com>
2023-07-20 14:46:26 -0300

    feat: use credentials provider to authorize user session on login

--- src/Utils/auth.ts
+++ src/Utils/auth.ts
@@ -1,6 +1,8 @@
 import { AuthOptions, getServerSession } from "next-auth";
-import { JWT } from "next-auth/jwt";
 import CredentialsProvider from "next-auth/providers/credentials";
+
+import { loginUser } from "@/services/users";
+import { AxiosError } from "axios";
 
 export const authOptions: AuthOptions = {
   providers: [
@@ -14,17 +16,17 @@ export const authOptions: AuthOptions = {
       async authorize(credentials, _req) {
         if (!(credentials?.email && credentials?.password)) return null;
 
-        // TODO: POST request to API to validate credentials
-
-        return {
-          id: "stub_id",
-          name: "john doe",
-          email: credentials.email,
-          token: "stub_token",
-          isAdmin: true,
-          avatar: "https://i.pravatar.cc/300",
-          created_at: new Date().toISOString(),
-        };
+        try {
+          // TODO: POST request to API to validate credentials
+          const data = await loginUser(credentials);
+          const { user, ...rest } = data;
+          return { ...rest, ...user };
+        } catch (error) {
+          if (error instanceof AxiosError) {
+            return Promise.reject(new Error(error.message));
+          }
+          return Promise.reject(new Error("Ocorreu um erro inesperado"));
+        }
       },
     }),
   ],
@@ -35,22 +37,30 @@ export const authOptions: AuthOptions = {
     strategy: "jwt",
   },
   callbacks: {
-    jwt({ token, user }: { token: JWT; user: any }) {
-      if (user) {
-        token.accessToken = user.token;
-        token.isAdmin = user.isAdmin;
-        token.sub = user.id;
+    jwt({ token, user }) {
+      console.log("user", user);
+
+      const tokenExpiresInNumber =
+        new Date(user.tokenExpiresIn).getTime() * 1000;
+      const refreshTokenExpiresInNumber =
+        new Date(user.refreshTokenExpiresIn).getTime() * 1000;
+
+      const newToken = {
+        ...token,
+        ...user,
+        tokenExpiresIn: tokenExpiresInNumber,
+        refreshTokenExpiresIn: refreshTokenExpiresInNumber,
+      };
+
+      if (Date.now() < newToken.tokenExpiresIn) {
+        console.log("Token válido", tokenExpiresInNumber);
+
+        return newToken;
       }
 
-      return { ...token, ...user };
+      return newToken;
     },
-    async session({ token, session }) {
-      if (token && session) {
-        session.user.id = token.sub as string;
-        session.user.isAdmin = token.isAdmin;
-        session.user.avatar = token.avatar;
-        session.user.created_at = token.created_at;
-      }
+    async session({ session }) {
       return { ...session };
     },
   },
