10a042589666b03dd2a923332005f1d2b3b65c2d
Charlie Bellow <charliebftm@gmail.com>
2023-07-24 14:28:14 -0300

    refactor: styling public group card component 2

--- src/app/(public)/categories/CategoryBannerPattern/CategoryBannerRoot.tsx
+++ src/app/(public)/categories/CategoryBannerPattern/CategoryBannerRoot.tsx
@@ -1,14 +1,14 @@
-import React, {ReactNode} from 'react'
+import React, { ReactNode } from "react";
 
 type CategoryBannerRootProps = {
-  children: ReactNode
-}
-export default function CategoryBannerRoot({children}: CategoryBannerRootProps) {
+  children: ReactNode;
+};
+export default function CategoryBannerRoot({
+  children,
+}: CategoryBannerRootProps) {
   return (
-    
     <div className="relative flex w-auto flex-col rounded-lg bg-white-100 shadow-card px-4 text-black-text">
       {children}
-
     </div>
   );
 }
