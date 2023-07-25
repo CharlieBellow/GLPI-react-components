62ec429488589be308ee19da597e915f1aaaf812
Enrique Santos <santosenrique869@gmail.com>
2023-07-25 11:04:31 -0300

    feat: refresh token when is expired

--- src/Utils/auth.ts
+++ src/Utils/auth.ts
@@ -1,8 +1,41 @@
+import { AxiosError } from "axios";
 import { AuthOptions, getServerSession } from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
 
-import { loginUser } from "@/services/users";
-import { AxiosError } from "axios";
+import { loginUser, refreshToken } from "@/services/users";
+import { UserModel } from "@/types";
+
+type TokenWithUserDetails = {
+  token: string;
+  refresh_token: string;
+  tokenExpiresIn: number;
+  refreshTokenExpiresIn: number;
+  user: UserModel;
+  error?: "RefreshTokenError";
+};
+
+async function refreshUserToken(
+  tokenObj: TokenWithUserDetails
+): Promise<TokenWithUserDetails> {
+  try {
+    const data = await refreshToken(tokenObj.refresh_token);
+    const tokenExpiresIn = Date.now() + 7 * 60 * 60 * 1000; // 7 days
+    const refreshTokenExpiresIn = Date.now() + 30 * 24 * 60 * 60 * 1000; // 30 days
+
+    return {
+      ...tokenObj,
+      token: data.refresh_token.token,
+      refresh_token: data.refresh_token.refresh_token,
+      tokenExpiresIn,
+      refreshTokenExpiresIn,
+    };
+  } catch (error) {
+    return {
+      ...tokenObj,
+      error: "RefreshTokenError",
+    };
+  }
+}
 
 export const authOptions: AuthOptions = {
   providers: [
@@ -17,7 +50,6 @@ export const authOptions: AuthOptions = {
         if (!(credentials?.email && credentials?.password)) return null;
 
         try {
-          // TODO: POST request to API to validate credentials
           const data = await loginUser(credentials);
           const { user, ...rest } = data;
           return { ...rest, ...user };
@@ -35,32 +67,37 @@ export const authOptions: AuthOptions = {
   },
   session: {
     strategy: "jwt",
+    maxAge: 7 * 60 * 60 * 24, // 7 days
   },
   callbacks: {
     jwt({ token, user }) {
-      console.log("user", user);
-
-      const tokenExpiresInNumber =
-        new Date(user.tokenExpiresIn).getTime() * 1000;
-      const refreshTokenExpiresInNumber =
-        new Date(user.refreshTokenExpiresIn).getTime() * 1000;
-
-      const newToken = {
-        ...token,
-        ...user,
-        tokenExpiresIn: tokenExpiresInNumber,
-        refreshTokenExpiresIn: refreshTokenExpiresInNumber,
-      };
-
-      if (Date.now() < newToken.tokenExpiresIn) {
-        console.log("Token válido", tokenExpiresInNumber);
-
-        return newToken;
+      if (token && user) {
+        return {
+          ...token,
+          user: {
+            id: user.id,
+            name: user.name!,
+            email: user.email!,
+            permissions: user.permissions,
+            roles: user.roles,
+            avatar: user.avatar,
+          },
+          token: user.token,
+          refresh_token: user.refresh_token,
+          tokenExpiresIn: new Date(user.refreshTokenExpiresIn).getTime(),
+          refreshTokenExpiresIn: new Date(user.refreshTokenExpiresIn).getTime(),
+        };
       }
 
-      return newToken;
+      if (Date.now() < token.tokenExpiresIn) {
+        return token;
+      }
+
+      return refreshUserToken(token);
     },
-    async session({ session }) {
+    async session({ session, token }) {
+      const { user, ...res } = token;
+      session.user = { ...user, ...res, email: user.email!, name: user.name! };
       return { ...session };
     },
   },
