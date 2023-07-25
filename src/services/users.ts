c02618aba815470be6900f61d26eae9aad7357e5
Enrique Santos <santosenrique869@gmail.com>
2023-07-25 11:22:35 -0300

    feat: create service to refresh token

--- src/services/users.ts
+++ src/services/users.ts
@@ -13,3 +13,16 @@ export async function loginUser(input: LoginUserInput) {
   const { data } = await api.post<LoginUserOutput>("/sessions", input);
   return data;
 }
+
+type RefreshToken = {
+  refresh_token: { token: string; refresh_token: string };
+};
+
+export async function refreshToken(refreshToken: string) {
+  const { data } = await api.post<RefreshToken>("/refresh-token", null, {
+    headers: {
+      "x-access-token": refreshToken,
+    },
+  });
+  return data;
+}
