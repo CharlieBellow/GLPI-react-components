b5f2f705ee6c2f58e97b5a3ee703b2e092268d09
Enrique Santos <santosenrique869@gmail.com>
2023-07-25 11:33:04 -0300

    feat: redirect user to login if occur error on refresh token

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,8 @@ import { Metadata } from "next";
 import { ReactNode } from "react";
 
 import { Providers } from "@/components/Providers";
+import { getAuthSession } from "@/Utils/auth";
+import { redirect } from "next/navigation";
 
 export const metadata: Metadata = {
   title: {
@@ -13,7 +15,17 @@ export const metadata: Metadata = {
   },
 };
 
-export default function RootLayout({ children }: { children: ReactNode }) {
+export default async function RootLayout({
+  children,
+}: {
+  children: ReactNode;
+}) {
+  const session = await getAuthSession();
+
+  if (session?.user.error === "RefreshTokenError") {
+    redirect("/login");
+  }
+
   return (
     <html>
       <body>
