476bca29e47e869271fcfd996b2aa413fce59a68
Enrique Santos <santosenrique869@gmail.com>
2023-07-25 13:24:13 -0300

    refactor: add SessionProvider in app entrypoint in pages folder

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,32 +1,38 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
-import { ChakraProvider } from '@chakra-ui/react';
-import  {AuthProvider} from '../Contexts/AuthContext';
-import { UserProvider } from '../Contexts/UserContext';
-import { MenuProvider } from '../Contexts/MenuContext';
-import 'react-toastify/dist/ReactToastify.css';
-import { ToastContainer } from 'react-toastify';
-import { MessageProvider } from '../Contexts/MessageContext';
-import { PreviousPageProvider } from '../Contexts/PreviousPageContext';
+import "../styles/globals.css";
+import "react-toastify/dist/ReactToastify.css";
 
-export default function App({ Component, pageProps }: AppProps) {
-  
+import type { AppProps as AppPropsNext } from "next/app";
+import { ChakraProvider } from "@chakra-ui/react";
+import { AuthProvider } from "../Contexts/AuthContext";
+import { UserProvider } from "../Contexts/UserContext";
+import { MenuProvider } from "../Contexts/MenuContext";
+import { ToastContainer } from "react-toastify";
+import { MessageProvider } from "../Contexts/MessageContext";
+import { PreviousPageProvider } from "../Contexts/PreviousPageContext";
+import { SessionProvider } from "next-auth/react";
+import { Session } from "next-auth";
+
+interface AppProps extends AppPropsNext {
+  session: Session;
+}
+
+export default function App({ Component, session, pageProps }: AppProps) {
   return (
-    <MessageProvider>
-    <AuthProvider>
-    <UserProvider>
-      <MenuProvider>
-        <ChakraProvider>
-          <PreviousPageProvider>                       
-              <>
-                <Component {...pageProps} />
-                <ToastContainer />
-              </>
-          </PreviousPageProvider> 
-        </ChakraProvider>
-      </MenuProvider>
-    </UserProvider>
-  </AuthProvider>
-  </MessageProvider>
-  ) 
+    <SessionProvider session={session}>
+      <MessageProvider>
+        <AuthProvider>
+          <UserProvider>
+            <MenuProvider>
+              <ChakraProvider>
+                <PreviousPageProvider>
+                  <Component {...pageProps} />
+                  <ToastContainer />
+                </PreviousPageProvider>
+              </ChakraProvider>
+            </MenuProvider>
+          </UserProvider>
+        </AuthProvider>
+      </MessageProvider>
+    </SessionProvider>
+  );
 }
