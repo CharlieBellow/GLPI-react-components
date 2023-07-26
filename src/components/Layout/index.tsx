d29e8b3c5b98b5e128d5c3ef74ee5c03f5a28c70
Enrique Santos <santosenrique869@gmail.com>
2023-07-26 09:45:41 -0300

    refactor: remove unused code

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,76 +1,27 @@
+import React from "react";
 
-import React, { useEffect, useState } from "react";
-import { useAuth } from "../../Contexts/AuthContext"
-import { useRouter } from "next/router";
-import LoadingPage from "../LoadingPage";
-import { usePreviousPage } from "../../Contexts/PreviousPageContext";
 import Sidebar from "../Sidebar/Sidebar";
 import { SidebarProvider } from "../Sidebar/SidebarContext";
 import CardMenu from "../Cards/CardMenu";
+
 interface PageProps extends React.HTMLAttributes<HTMLElement> {
   children: React.ReactNode;
 }
 
 // * layout padrão das páginas
 export function Layout({ children, ...rest }: PageProps) {
-  const router = useRouter();
-  const [loaded, setLoaded] = useState(false);
-  const { auth, user, verifyCookies } = useAuth()
-  const { page, changePage, justLogged, changeLogged } = usePreviousPage()
-
-  useEffect(() => {
-    if (loaded) {
-      if (!auth) {
-        
-        changePage(router.asPath)
-        router.push('/login')
-      } else {
-        if (justLogged || router.asPath.includes("login")) {
-          changeLogged(false)
-          if (page !== "") {
-            console.log("redirecting by context")
-            changePage("")
-            router.push(page)
-          } else {
-            router.push('/Dashboard')
-          }
-        }
-      }
-    }
-
-
-  }, [auth, changeLogged, changePage, justLogged, loaded, page, router])
-
-  useEffect(() => {
-    setTimeout(() => {
-      verifyCookies()
-      setLoaded(true)
-    }, 300);
-  }, [])
-  if (loaded && (router.asPath.includes("login"))) {
-    return(
-      <main>
-        {children}
-      </main>
-    )
-  }else if (loaded && (!router.asPath.includes("login") && auth)){
-    return (
-      <SidebarProvider>
-        <Sidebar>
-          <>
-            <div className="flex flex-col bg-slate-200">
-                <CardMenu pagetitle="Dashboard"/>
-                <div {...rest} className="content z-30 bg-gray-medium">
-							    {children}
-						    </div>
+  return (
+    <SidebarProvider>
+      <Sidebar>
+        <>
+          <div className="flex flex-col bg-slate-200">
+            <CardMenu pagetitle="Dashboard" />
+            <div {...rest} className="content z-30 bg-gray-medium">
+              {children}
             </div>
-            </>
-        </Sidebar>
-      </SidebarProvider>
-    )
-  } else {
-    return (<LoadingPage />)
-  }
+          </div>
+        </>
+      </Sidebar>
+    </SidebarProvider>
+  );
 }
-
-
