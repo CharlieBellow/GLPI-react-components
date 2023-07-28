19953691797f66c80d68a7889d98e0c73c2003ea
Enrique Santos <santosenrique869@gmail.com>
2023-07-28 09:01:04 -0300

    feat: create interceptor to add token in requests

--- src/Utils/server/api.ts
+++ src/Utils/server/api.ts
@@ -1,5 +1,16 @@
 import axios from "axios";
+import { getAuthSession } from "@/Utils/auth";
 
 export const api = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL,
+});
+
+api.interceptors.request.use(async (config) => {
+  const session = await getAuthSession();
+
+  if (session) {
+    config.headers["Authorization"] = `Bearer ${session.user.token}`;
+  }
+
+  return config;
 });
