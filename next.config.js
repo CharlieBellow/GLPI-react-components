b4cebf8bc4c74d3d806241b6b23493cbf88af587
Enrique Santos <santosenrique869@gmail.com>
2023-08-01 07:51:33 -0300

    chore: remove experimental type routes

--- next.config.js
+++ next.config.js
@@ -2,17 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "www.github.com",
-        port: "",
-        pathname: "/**",
-      },
-    ],
-  },
-  experimental: {
-    typedRoutes: true,
+    domains: ["www.github.com"],
   },
 };
 
