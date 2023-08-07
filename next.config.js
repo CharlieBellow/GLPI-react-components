1a79a0d012759b959c94e77535195bb6d184b4aa
Charlie Bellow <charliebftm@gmail.com>
2023-08-07 08:03:39 -0300

    refactor: updating next.config

--- next.config.js
+++ next.config.js
@@ -1,20 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-
   images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "www.github.com",
-        port: "",
-        pathname: "/**",
-      },
-    ],
-    domains: ["github.com"],
-  },
-  experimental: {
-    typedRoutes: true,
     domains: ["www.github.com"],
   },
 };
