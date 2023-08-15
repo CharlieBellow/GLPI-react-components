606a05660eeaae943382c57bed14ce42bbc23473
Enrique Santos <santosenrique869@gmail.com>
2023-08-15 10:01:03 -0300

    refactor: change shape of functions

--- src/Utils/server/postInfo.tsx
+++ src/Utils/server/postInfo.tsx
@@ -1,51 +1,25 @@
-import axios from "axios";
-
 import { api } from "@/Utils/server/api";
-// * funções de adicionar no servidor
 
-const baseURL = "http://172.27.12.171:3333";
-// TODO: falta gerenciar erros nas requisições
-export function postGroup(values: object, token: string) {
-  axios({
-    method: "post",
-    baseURL: baseURL,
-    url: `/servicebook/group/`,
+type CreateGroup = {
+  description: string;
+};
 
-    data: values,
-    headers: { authorization: `Bearer ${token}` },
-  });
+export function postGroup(values: CreateGroup): Promise<void> {
+  return api.post(`/servicebook/group`, values);
 }
 
 export async function postSubGroup(values: object): Promise<void> {
   return api.post(`/servicebook/subgroup`, values);
 }
 
-export function postService(values: object, token: string) {
-  axios({
-    method: "post",
-    baseURL: baseURL,
-    url: `/servicebook/service/`,
-    data: values,
-    headers: { authorization: `Bearer ${token}` },
-  });
+export async function postService(values: object): Promise<void> {
+  return api.post("/servicebook/service", values);
 }
 
-export function postServiceOrder(values: object, token: string) {
-  axios({
-    method: "post",
-    baseURL: baseURL,
-    url: `/servicebook/serviceorder/`,
-    data: values,
-    headers: { authorization: `Bearer ${token}` },
-  });
+export async function postServiceOrder(values: object): Promise<void> {
+  return api.post("/servicebook/serviceorder", values);
 }
 
-export function postUser(values: object, token: string) {
-  axios({
-    method: "post",
-    baseURL: baseURL,
-    url: `/users`,
-    data: values,
-    headers: { authorization: `Bearer ${token}` },
-  });
+export async function postUser(values: object): Promise<void> {
+  return api.post("/users", values);
 }
