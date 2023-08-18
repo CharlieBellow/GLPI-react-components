664e4d071fe85199551472d290a51a33bb7fdcf4
Enrique Santos <santosenrique869@gmail.com>
2023-08-18 10:01:30 -0300

    refactor: remove chakra-ui provider

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -4,16 +4,13 @@ import { ReactNode } from "react";
 
 import { SessionProvider } from "next-auth/react";
 
-import { ChakraProvider } from "@chakra-ui/react";
 import { ToastContainer } from "react-toastify";
 
 export function Providers({ children }: { children: ReactNode }) {
   return (
     <SessionProvider>
-      <ChakraProvider>
-        {children}
-        <ToastContainer />
-      </ChakraProvider>
+      {children}
+      <ToastContainer />
     </SessionProvider>
   );
 }
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,10 @@
+import "@/styles/globals.css";
 import "react-toastify/dist/ReactToastify.css";
-import "../styles/globals.css";
 
 import type { AppProps as AppPropsNext } from "next/app";
 import { Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
 
-import { ChakraProvider } from "@chakra-ui/react";
 import { ToastContainer } from "react-toastify";
 
 interface AppProps extends AppPropsNext {
@@ -15,11 +14,8 @@ interface AppProps extends AppPropsNext {
 export default function App({ Component, session, pageProps }: AppProps) {
   return (
     <SessionProvider session={session}>
-      <ChakraProvider>
-        <Component {...pageProps} />
-        <div id="portal" />
-        <ToastContainer />
-      </ChakraProvider>
+      <Component {...pageProps} />
+      <ToastContainer />
     </SessionProvider>
   );
 }
