51eea270ad432084ed4ca932a12e05f91a4e431a
Enrique Santos <santosenrique869@gmail.com>
2023-08-24 14:38:22 -0300

    feat: handle api error with interceptors

--- src/Utils/server/api.ts
+++ src/Utils/server/api.ts
@@ -1,6 +1,9 @@
 import { getSession } from "next-auth/react";
 
-import axios from "axios";
+import axios, { AxiosError, isAxiosError } from "axios";
+
+import { DEFAULT_ERRORS } from "@/exceptions/default-errors";
+import { ApiError, HttpError } from "@/exceptions/http-error";
 
 export const api = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL,
@@ -15,3 +18,32 @@ api.interceptors.request.use(async (config) => {
 
   return config;
 });
+
+api.interceptors.response.use(
+  (res) => res,
+  (error) => {
+    if (!isAxiosError(error)) {
+      return Promise.reject(
+        new HttpError(
+          "Um erro inesperado ocorreu. Por favor, tente novamente mais tarde."
+        )
+      );
+    }
+
+    const errorApi = error as AxiosError<ApiError>;
+
+    if (errorApi.response?.status === 500) {
+      return Promise.reject(new HttpError(DEFAULT_ERRORS[500]));
+    }
+
+    if (errorApi.response?.status === 404) {
+      return Promise.reject(new HttpError(DEFAULT_ERRORS[404]));
+    }
+
+    if (!errorApi.response?.data.message) {
+      return Promise.reject(new HttpError(DEFAULT_ERRORS.generic));
+    }
+
+    return Promise.reject(new HttpError(errorApi.response.data.message));
+  }
+);
