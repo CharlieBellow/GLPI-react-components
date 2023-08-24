0918d00da9ee579308cc6d1a4786b41353383ee3
Enrique Santos <santosenrique869@gmail.com>
2023-08-24 14:41:24 -0300

    refactor: use tailwind variants to create variants instead conditional blocks

--- src/components/Buttons/Button.tsx
+++ src/components/Buttons/Button.tsx
@@ -1,121 +1,89 @@
-"use client";
+import { ButtonHTMLAttributes, forwardRef } from "react";
 
-import { ButtonHTMLAttributes } from "react";
+import { tv, VariantProps } from "tailwind-variants";
 
-import { Spinner } from "@chakra-ui/react";
-
+import { Spinner } from "@/components/icons";
 import * as Icon from "@/components/icons";
 
-interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
-  title?: string;
-  icon?: Icon.IconProps;
-  theme:
-    | "primary"
-    | "secondary"
-    | "tertiary"
-    | "primaryAction"
-    | "secondaryAction"
-    | "textOnly"
-    | "primaryMobile"
-    | "withIcon"
-    | "dropdowMenu"
-    | "primaryActionWithIcon"
-    | "secondaryActionWithIcon"
-    | "tertiaryMobile"
-    | "tertiaryActionWithIcon";
-  disabled?: boolean;
-  isSubmitting?: boolean;
-}
+const button = tv({
+  base: "flex w-full cursor-pointer items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-bold shadow-button transition-colors focus-visible:outline-2 focus-visible:outline-offset-2",
+  variants: {
+    theme: {
+      primary:
+        "bg-primary-blue text-white-100 hover:bg-primary-blue/80 focus:bg-primary-blue/80 focus-visible:outline-primary-blue",
+      secondary:
+        "bg-tertiary-1 text-white-100 hover:bg-tertiary-1/80 focus-visible:outline-tertiary-1/80",
+      tertiary:
+        "bg-secondary-2 text-tertiary-1 hover:bg-secondary-2/80 focus-visible:outline-secondary-2",
+      textOnly:
+        "text-primary-blue hover:bg-primary-blue/70 focus-visible:outline-none",
+      warning:
+        "bg-primary-red text-white-100 hover:bg-primary-red/80 focus-visible:outline-primary-red",
+      danger:
+        "bg-danger text-tertiary-2 hover:bg-danger/80 focus-visible:outline-danger",
+    },
+    loading: {
+      true: "pointer-events-none",
+    },
+    disabled: {
+      true: "pointer-events-none opacity-50",
+    },
+    size: {
+      sm: "h-8 px-2",
+      md: "h-11 px-4",
+      lg: "h-12 px-6",
+    },
+  },
+  defaultVariants: {
+    size: "md",
+    theme: "primary",
+  },
+});
 
-export function Button({ isSubmitting, ...props }: ButtonProps) {
-  return (
-    <button
-      {...props}
-      type={props.type}
-      className={`
-				rounded-md font-bold focus:outline-blue-ufal
-				${
-          props.theme === "primary"
-            ? "flex h-12 items-center justify-center gap-1 bg-blue-ufal p-2 text-base text-white-100 shadow-button hover:bg-blue-ufal-hover focus:outline-light-bg lg:px-13"
-            : ""
-        }
-				${
-          props.theme === "secondary"
-            ? "flex h-12 items-center gap-2 bg-gray-text p-2 text-base text-white-100 shadow-button hover:bg-opacity-80 lg:px-10"
-            : ""
-        }
-				${
-          props.theme === "tertiary"
-            ? // text- base for text-xl
-              "h-12 bg-gray-medium px-13 py-2 text-base text-gray-text shadow-button hover:bg-opacity-80"
-            : ""
-        }
-				${
-          props.theme === "tertiaryMobile"
-            ? "bg-gray-medium px-5 py-2 text-sm text-gray-text shadow-button"
-            : ""
-        }
-        ${
-          props.theme === "primaryAction"
-            ? "h-11 w-24 bg-blue-ufal px-2.5 py-2 text-base text-white-100 shadow-button hover:bg-blue-ufal-hover focus:outline-light-bg"
-            : ""
-        }
-				${
-          props.theme === "secondaryAction"
-            ? "h-11 w-22 bg-gray-medium px-2.5 py-2 text-base text-gray-text shadow-button hover:bg-opacity-80"
-            : ""
-        }
-				${
-          props.theme === "textOnly"
-            ? "w-full bg-transparent py-4 text-base text-blue-ufal hover:bg-opacity-80"
-            : ""
-        }
-				${
-          props.theme === "primaryMobile"
-            ? "bg-blue-ufal px-5 py-2 text-sm text-white-100 shadow-button hover:bg-blue-ufal-hover focus:outline-light-bg lg:px-25 lg:py-4 lg:text-base"
-            : ""
-        }
-				${
-          props.theme === "withIcon"
-            ? `relative flex h-15 min-w-[3.75rem] flex-row-reverse items-center justify-center gap-2 rounded-full bg-blue-ufal p-4 text-base text-white-100
-					shadow-button hover:bg-blue-ufal-hover focus:outline-light-bg lg:h-10 lg:rounded-md lg:px-3 lg:py-6${props.className}`
-            : ""
-        }
-				${
-          props.theme === "dropdowMenu"
-            ? `relative flex h-15 min-w-[3.75rem] flex-row-reverse items-center justify-center gap-2 rounded-full bg-blue-ufal p-4 text-base text-white-100
-					shadow-button hover:bg-blue-ufal-hover focus:outline-light-bg lg:h-10 lg:rounded-full lg:px-3 lg:py-6${props.className}`
-            : ""
-        }
-				${
-          props.theme === "primaryActionWithIcon"
-            ? `flex items-center justify-center gap-2 bg-blue-ufal p-4 text-xl font-bold text-white-100 shadow-button hover:bg-blue-ufal-hover
-					focus:outline-light-bg`
-            : ""
-        }
-				${
-          props.theme === "secondaryActionWithIcon"
-            ? `flex items-center justify-center gap-2 bg-gray-text p-4 text-xl font-bold text-white-100 shadow-button
-					hover:bg-opacity-80`
-            : ""
-        }
-				${
-          props.theme === "tertiaryActionWithIcon"
-            ? `flex items-center justify-center gap-2 bg-gray-medium p-4 text-xl font-bold text-gray-text shadow-button
-					hover:bg-opacity-80`
-            : ""
-        }
-				${
-          props.disabled === true
-            ? "bg-gray-text opacity-60 hover:bg-gray-text hover:opacity-60"
-            : ""
-        }
-			`}
-    >
-      <>
-        <>{props.icon}</>
-        {isSubmitting ? <Spinner size="md" /> : props.title}
-      </>
-    </button>
-  );
-}
+type ButtonProps2 = ButtonHTMLAttributes<HTMLButtonElement> &
+  VariantProps<typeof button> & {
+    isLoading?: boolean;
+    isSubmitting?: boolean;
+    title?: string;
+    icon?: Icon.IconProps;
+  };
+
+export const Button = forwardRef<HTMLButtonElement, ButtonProps2>(
+  (
+    {
+      size,
+      theme,
+      title,
+      className,
+      isLoading = false,
+      isSubmitting = false,
+      disabled,
+      children,
+      ...props
+    },
+    ref
+  ) => {
+    return (
+      <button
+        {...props}
+        ref={ref}
+        className={button({
+          size,
+          theme,
+          loading: isSubmitting || isLoading,
+          disabled: disabled,
+          className,
+        })}
+      >
+        {isSubmitting ? (
+          <Spinner size={20} className="animate-spin text-current" />
+        ) : title ? (
+          title
+        ) : (
+          children
+        )}
+      </button>
+    );
+  }
+);
+Button.displayName = "Button";
