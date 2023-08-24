6d476fc1df8f8f00e92f6853b77355f667d2c4d6
Enrique Santos <santosenrique869@gmail.com>
2023-08-24 14:32:57 -0300

    feat: add components from shadcn-ui

--- src/components/ui/DropDownMenu.tsx
+++ src/components/ui/DropDownMenu.tsx
@@ -3,7 +3,7 @@
 import * as React from "react";
 
 import * as DropdownMenuPrimitive from "@radix-ui/react-dropdown-menu";
-import { Check, ChevronRight, Circle } from "lucide-react";
+import { ArrowRight, Check, Circle } from "phosphor-react";
 
 import { cn } from "@/Utils/cn";
 
@@ -35,7 +35,7 @@ const DropdownMenuSubTrigger = React.forwardRef<
     {...props}
   >
     {children}
-    <ChevronRight className="ml-auto h-4 w-4" />
+    <ArrowRight className="ml-auto h-4 w-4" />
   </DropdownMenuPrimitive.SubTrigger>
 ));
 DropdownMenuSubTrigger.displayName =
