57ebc6f261ba968da3af15826eecec7e48885f5f
Enrique Santos <santosenrique869@gmail.com>
2023-08-24 14:35:09 -0300

    feat: create hook to handle api erros

---
+++ src/hooks/useHandleApiError.ts
@@ -0,0 +1,30 @@
+import { useCallback, useEffect, useState } from "react";
+
+import { useMessage } from "@/Contexts/MessageContext";
+import { DEFAULT_ERRORS } from "@/exceptions/default-errors";
+import { HttpError } from "@/exceptions/http-error";
+
+/**
+ * @description Provide a error and this hook will handle it and show a message to user
+ */
+export function useHandleApiError() {
+  const [error, setError] = useState<string | null>(null);
+  const { errorMessage } = useMessage();
+
+  const handleError = useCallback((err: unknown) => {
+    if (err instanceof HttpError) {
+      setError(err.message);
+      return;
+    }
+    setError(DEFAULT_ERRORS.generic);
+  }, []);
+
+  useEffect(() => {
+    if (error) {
+      errorMessage(error);
+      setError(null);
+    }
+  }, [error, errorMessage]);
+
+  return handleError;
+}
