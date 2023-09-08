40dbf87fbce2a93c631526aaab9ec8750b74cb60
Enrique Santos <santosenrique869@gmail.com>
2023-09-08 11:13:10 -0300

    feat: create forgot-password page

---
+++ src/app/(public)/(routes)/(auth)/forgot-password/page.tsx
@@ -0,0 +1,5 @@
+import { FormForgotPassword } from "@/app/(public)/(routes)/(auth)/forgot-password/components/FormForgotPassword";
+
+export default function ForgotPasswordPage() {
+  return <FormForgotPassword />;
+}
